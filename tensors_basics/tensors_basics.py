a862beddae811545447620857d33acb569c1a375
HarryAri <peter.harry.ari@gmail.com>
2024-11-07 13:29:40 +0100

    Added comments and modified print statements

--- tensors_basics/tensors_basics.py
+++ tensors_basics/tensors_basics.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 
+# Creating rank 0 tensor (scalar) that is containing datatype string
 string = tf.Variable("Hello there", tf.string)
+
+# Creating rank 0 tensor (scalar) that is containing datatype int
 number = tf.Variable(123, tf.int64)
+
+# Creating rank 0 tensor (scalar) that is containing datatype float
 floating = tf.Variable(3.123, tf.float64)
 
-print(string)
-print(number)
-print(floating)
+print(f"Rank 0 tensor (scalar) containing string: {string}")
+print(f"Rank 0 tensor (scalar) containing string: {number}")
+print(f"Rank 0 tensor (scalar) containing float: {floating}")
